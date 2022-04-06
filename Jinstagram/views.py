e4c282b74ac8fa4468abcf91161d6243ff5b583d
seungjun-Noh <stw550@gmail.com>
2022-04-07 00:13:34 +0900

    피드화면

---
+++ Jinstagram/views.py
@@ -0,0 +1,10 @@
+import imp
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+class Sub(APIView):
+    def get(self, request):
+        return render(request, "jinstagram/main.html")
+    
+    def post(self, request):
+        return render(request, "jinstagram/main.html")
